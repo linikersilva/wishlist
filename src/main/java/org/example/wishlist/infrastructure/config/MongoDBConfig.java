43314bd9903554c273f5ac472a8ede3a51a61122
Liniker Silva <linikergabrieldasilva@gmail.com>
2024-09-07 11:12:55 -0300

    configuring database

---
+++ src/main/java/org/example/wishlist/infrastructure/config/MongoDBConfig.java
@@ -0,0 +1,21 @@
+package org.example.wishlist.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDBConfig {
+
+    @Bean
+    public MongoDatabaseFactory mongoConfigure() {
+        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/wishlist");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoConfigure());
+    }
+}
